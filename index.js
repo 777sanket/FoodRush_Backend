7f7dedb1f13a28cb3256b522747824c11312117d
777sanket <sanketwasnik1902@gmail.com>
2024-11-29 16:03:48 +0530

    feat: first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import express from "express";
+import cors from "cors";
+import foodRouter from "./src/routers/food.router.js";
+import userRouter from "./src/routers/user.router.js";
+import orderRouter from "./src/routers/order.router.js";
+
+import { dbconnect } from "./src/config/database.config.js";
+dbconnect();
+
+const app = express();
+app.use(express.json());
+
+// app.get("/", (req, res) => res.status(200).json({ message: "Hello World!" }));
+app.use(
+  cors({
+    credentials: true,
+    origin: ["http://localhost:3001"],
+    // origin: ["https://food-rush-frontend.vercel.app"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
+
+app.use("/api/foods", foodRouter);
+app.use("/api/users", userRouter);
+app.use("/api/orders", orderRouter);
+
+const PORT = 5001;
+app.listen(PORT, () => {
+  console.log("listening on port " + PORT);
+});
---
+++ src/data.js
@@ -0,0 +1,102 @@
+export const sample_foods = [
+    {
+      name: 'Pizza Pepperoni',
+      cookTime: '10-20',
+      price: 10,
+      favourite: false,
+      origins: ['italy'],
+      stars: 4.5,
+      imageUrl: 'food-1.jpg',
+      tags: ['FastFood', 'Pizza', 'Lunch'],
+    },
+    {
+      name: 'Meatball',
+      price: 20,
+      cookTime: '20-30',
+      favourite: true,
+      origins: ['persia', 'middle east', 'china'],
+      stars: 5,
+      imageUrl: 'food-2.jpg',
+      tags: ['SlowFood', 'Lunch'],
+    },
+    {
+      name: 'Hamburger',
+      price: 5,
+      cookTime: '10-15',
+      favourite: false,
+      origins: ['germany', 'us'],
+      stars: 3.5,
+      imageUrl: 'food-3.jpg',
+      tags: ['FastFood', 'Hamburger'],
+    },
+    {
+      name: 'Fried Potatoes',
+      price: 2,
+      cookTime: '15-20',
+      favourite: true,
+      origins: ['belgium', 'france'],
+      stars: 3,
+      imageUrl: 'food-4.jpg',
+      tags: ['FastFood', 'Fry'],
+    },
+    {
+      name: 'Chicken Soup',
+      price: 11,
+      cookTime: '40-50',
+      favourite: false,
+      origins: ['india', 'asia'],
+      stars: 3.5,
+      imageUrl: 'food-5.jpg',
+      tags: ['SlowFood', 'Soup'],
+    },
+    {
+      name: 'Vegetables Pizza',
+      price: 9,
+      cookTime: '40-50',
+      favourite: false,
+      origins: ['italy'],
+      stars: 4.0,
+      imageUrl: 'food-6.jpg',
+      tags: ['FastFood', 'Pizza', 'Lunch'],
+    },
+    {
+      name: 'Maggie',
+      price: 20,
+      cookTime: '15-20',
+      favourite: true,
+      origins: ['italy', 'india'],
+      stars: 4.5,
+      imageUrl: 'food-7.jpg',
+      tags: ['FastFood'],
+    },
+    
+  ];
+
+
+export const sample_tags = [
+    { name: 'All', count: 7 },
+    { name: 'FastFood', count: 5 },
+    { name: 'Pizza', count: 2 },
+    { name: 'Lunch', count: 3 },
+    { name: 'SlowFood', count: 2 },
+    { name: 'Hamburger', count: 1 },
+    { name: 'Fry', count: 1 },
+    { name: 'Soup', count: 1 },
+];
+
+export const sample_users = [
+  {
+    name: 'John Doe',
+    email: 'john@gmail.com',
+    password: '12345',
+    address: 'Toronto On',
+    isAdmin: false,
+  },
+  {
+    name: 'Jane Doe',
+    email: 'jane@gmail.com',
+    password: '12345',
+    address: 'Shanghai',
+    isAdmin: true,
+  },
+];
